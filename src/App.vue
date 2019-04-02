f721520e597426e0336d72e3c58a972fcf4dd3da
Christopher Bloom <bloomcb@gmail.com>
2019-04-02 08:57:47 -0700

    First commit to workout maker

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="jumbotron jumbotron-fluid text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h1 class="display-4">Workout Maker</h1>
+            <button @click="random" class="btn btn-primary">
+              Make Combined Workout
+            </button>
+            <div class="my-4">
+              <span
+                v-for="movement in sample"
+                :key="movement"
+                class="mx-1 badge badge-dark"
+              >
+                {{ movement }}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Upper</h1>
+          <ul>
+            <li v-for="top in upper" :key="top">
+              {{ top }}
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>Lower</h1>
+          <ul>
+            <li v-for="bottom in lower" :key="bottom">
+              {{ bottom }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { sampleSize } from 'lodash';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      upper: [
+        'bench press',
+        'curls',
+        'push ups',
+        'overhead press',
+        'pull ups',
+        'shoulder raise',
+      ],
+      lower: [
+        'squats',
+        'bulgarian split squats',
+        'deadlifts',
+        'ham curls',
+        'calf raises',
+        'box jumps',
+      ],
+      sample: [],
+    };
+  },
+  methods: {
+    random() {
+      this.sample = [
+        ...sampleSize(this.upper, 3),
+        ...sampleSize(this.lower, 3),
+      ];
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
